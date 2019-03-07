2a6c79a5a6b992d7a5b5031046ae39ca457fe10b
Fabien Garcia <fabien.garcia@enac.fr>
2019-03-07 10:10:27 +0100

    Added files for GPL-3.0-or-later

--- src/Runnable.cpp
+++ src/Runnable.cpp
@@ -1,6 +1,21 @@
-//
-// Created by garciafa on 01/03/19.
-//
+/*
+ * Copyright 2019 Fabien Garcia
+ * This file is part of ModemTester
+ *
+ * ModemTester is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ModemTester is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ModemTester.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 
 #include <functional>
 #include <iostream>
